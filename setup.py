23503921fde1833ac8c28041f84c8fddcab6a14d
Your Name <your-email@example.com>
2025-11-22 00:06:03 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="generative-ai-medical-chatbot",
+    version="0.1.0",
+    author="Suman Sharma", 
+    author_email='sumankumarsharma.2215@gmail.com', 
+    packages= find_packages(), 
+    install_requires = []
+)
